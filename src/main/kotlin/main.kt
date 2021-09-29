3eb6d0a2c5a8e016bdb4f55ecb734bb8d2c32b34
mykastor <loveviski1@gmail.com>
2021-09-29 19:30:07 +0300

    создал класс DataBaseClass. Реализовал функции add/remove

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,63 +1,100 @@
 import java.io.File
 
-typealias DataBase = MutableMap<String, String>
-const val PATH_TO_DB = "database/db.txt"
 const val BOUND_TO_REBUILD = 1e5
 
-data class LoadDataBaseResult(val db : DataBase,
-                              val needToRebuild: Boolean)
+typealias DataBase = MutableMap<String, String>
 
-fun initDataBase() {
-    if (!File(PATH_TO_DB).exists()) {
-        File(PATH_TO_DB).appendText("init database123\n")
+data class DataBaseClass(val PATH_TO_DB : String) {
+
+    private val db = loadDataBase()
+
+    fun addToFile(str: String) {
+        File(PATH_TO_DB).appendText(str)
     }
-}
 
-fun removeDataBase() {
-    assert(File(PATH_TO_DB).exists()) {
-        "removeDataBase used when there is no database"
-    }
-    File(PATH_TO_DB).writeText("init database\n")
-}
-
-fun loadDataBase() : LoadDataBaseResult {
-    val lines = File(PATH_TO_DB).readLines()
-    val db = mutableMapOf<String, String>()
-    var deleteCount = 0
-    lines.forEach {
-        val str = it.split(' ')
-        when (str[0]) {
-            "add" -> {
-                db[str[1]] = str[2]
-            }
-            "delete" -> {
-                deleteCount += 1
-                db.remove(str[1])
-            }
+    private fun initDataBase() {
+        if (!File(PATH_TO_DB).exists()) {
+            File(PATH_TO_DB).appendText("init database123\n")
         }
     }
-    return LoadDataBaseResult(db, deleteCount >= BOUND_TO_REBUILD)
-}
 
-fun rebuildDataBase(db : DataBase) {
-    // TODO
-}
+    fun removeDataBase() {
+        assert(File(PATH_TO_DB).exists()) {
+            "removeDataBase used when there is no database"
+        }
+        File(PATH_TO_DB).writeText("init database\n")
+    }
 
-fun main(args: Array<String>) {
-    initDataBase()
-    removeDataBase()
-    val (db, needToRebuild) = loadDataBase()
-    if (needToRebuild) {
+    private fun askToRebuild() {
         println("do you want to rebuild database? (it might take some time) [y; n]")
         println("it happened because the program did not close with the \"exit\" command")
         val str = readLine() ?: return
         if (str.first().lowercase() == "y") {
-            rebuildDataBase(db)
+            rebuildDataBase()
         }
     }
-    /*
-    while (true) {
-        val str = readLine() ?: return
+
+    fun add(key : String, value : String) {
+        db[key] = value
+        addToFile("add $key $value\n")
     }
-     */
+
+    fun remove(key : String) {
+        if (db.containsKey(key)) {
+            db.remove(key)
+            addToFile("delete $key\n")
+        }
+    }
+
+    private fun loadDataBase(): DataBase {
+        initDataBase()
+        val lines = File(PATH_TO_DB).readLines()
+        val db = mutableMapOf<String, String>()
+        var deleteCount = 0
+        lines.forEach {
+            val str = it.split(' ')
+            when (str[0]) {
+                "add" -> {
+                    db[str[1]] = str[2]
+                }
+                "delete" -> {
+                    deleteCount += 1
+                    db.remove(str[1])
+                }
+            }
+        }
+        if (deleteCount >= BOUND_TO_REBUILD) {
+            askToRebuild()
+        }
+        return db
+    }
+
+    private fun rebuildDataBase() {
+        // TODO
+    }
+}
+
+fun main(args: Array<String>) {
+    val db = DataBaseClass("database/db.txt")
+
+    while (true) {
+        val cmd = readLine() ?: return
+        val str = cmd.split(' ')
+        when (str[0]) {
+            "add" -> {
+                if (str.size != 3) {
+                    println("Please, enter input format in this format: \"add key value\".\nNotice that key and value cannot have a whitespace")
+                    continue
+                }
+                db.add(str[1], str[2])
+            }
+            "remove" -> {
+                if (str.size != 2) {
+                    println("Please, enter input format in this format: \"remove key\".\nNotice that key cannot have a whitespace")
+                    continue
+                }
+                db.remove(str[1])
+            }
+        }
+    }
 }
