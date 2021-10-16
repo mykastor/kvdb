5ba69e6c0603162ab850e5f6c44bf3c08451e4e5
mykastor <loveviski1@gmail.com>
2021-10-16 19:39:27 +0300

    добавил catch в main

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -13,16 +13,21 @@ fun main(args: Array<String>) {
     while (true) {
         val cmd = readLine() ?: return
         val str = cmd.split(' ')
-        when (str[0]) {
-            "add" -> add(db, str)
-            "remove" -> remove(db, str)
-            "find" -> find(db, str)
-            "exit" -> exitProcess(0)
-            "clean!!" -> db.cleanDataBase()
-            "help" -> help()
-            "changepath" -> changePath(db, str)
-            "path?" -> println(db.pathToDatabase)
-            else -> println("Wrong command. Use \"help\" to see available commands")
+        try {
+            when (str[0]) {
+                "add" -> add(db, str)
+                "remove" -> remove(db, str)
+                "find" -> find(db, str)
+                "exit" -> exitProcess(0)
+                "clean!!" -> db.cleanDataBase()
+                "help" -> help()
+                "changepath" -> changePath(db, str)
+                "path?" -> println(db.pathToDatabase)
+                else -> println("Wrong command. Use \"help\" to see available commands")
+            }
+        } catch (e: Exception) {
+            logger.error(e) {e}
+            println(e)
         }
     }
 }
